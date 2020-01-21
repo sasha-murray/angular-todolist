758f256964338ef281cc10bce5017d932504e7b1
sasha-murray <sasha.murray@appliedtechnology.se>
2020-01-21 13:45:55 +0100

    Broke out footer

---
+++ src/app/todo-list-footer/todo-list-footer.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Todo } from '../todo';
+
+@Component({
+  selector: 'app-todo-list-footer',
+  templateUrl: './todo-list-footer.component.html',
+  styleUrls: ['./todo-list-footer.component.scss']
+})
+export class TodoListFooterComponent implements OnInit {
+
+  @Input()
+  todos: Todo[];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
